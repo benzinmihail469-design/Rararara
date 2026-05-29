16b9f278ed816a50fef0b925675bcb7d3287feb5
benzinmihail469-design <benzinmihail469@gmail.com>
2026-05-30 00:38:13 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -2,10 +2,12 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local TweenService = game:GetService("TweenService")
 
-white true do
 function speed(a, b) 
     return a + b
-end
+end   
+    
+
+white true do 
     local result = speed(100, 100) 
     print ("result") 
     task.wait (0.5) 
