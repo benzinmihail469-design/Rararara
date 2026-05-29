c7edbf93f5a2e9779fbb406c30fa96cd988ee745
benzinmihail469-design <benzinmihail469@gmail.com>
2026-05-29 23:28:06 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -3,7 +3,7 @@ local LocalPlayer = Players.LocalPlayer
 local TweenService = game:GetService("TweenService")
 
 local speed = 100
-if speed == 10000000 then 
+if speed <= 10000000 then 
     print("есть") 
 else
     print("нету") 
