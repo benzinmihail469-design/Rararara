0c0d91d58dbe0704d5fdfbd9553bc9c2004959c1
benzinmihail469-design <benzinmihail469@gmail.com>
2026-05-30 23:21:26 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,3 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("Name", "RJTheme3")
+
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
+    print(currentOption)
+end)
