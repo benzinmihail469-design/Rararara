4a1b39d40cc54a210885e8d566a58a5441eafbcc
benzinmihail469-design <benzinmihail469@gmail.com>
2026-06-01 00:57:25 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,19 +1,16 @@
-local Players = game:GetService("Players") 
-local localplayer = Players:localplayer
-local character = localplayer:Character
-local humanoid = Character:Waitforchild("humanoid")
+local Players = game:GetService("Players")
+local localplayer = Players.LocalPlayer
+local character = localplayer.Character or localplayer.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
 
 local speed = 1000000
-while true do 
-    humanoid.walkspeed -= speed then
-    humanoid.walkspeed = speed 
-    print("скорость нету.. speed") 
-else
-    print("скорость есть.. humanoid.walkspeed") 
+
+while true do
+    if humanoid.WalkSpeed ~= speed then
+        humanoid.WalkSpeed = speed
+        print("Скорость установлена на " .. speed)
+    else
+        print("Скорость уже " .. humanoid.WalkSpeed)
+    end
+    task.wait(0.05)
 end
-task.wait(0.05)
-end
-
-
-
-    
