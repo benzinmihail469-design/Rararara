74b6febc0fb54589d9ac58928ad19be66d391eb2
benzinmihail469-design <benzinmihail469@gmail.com>
2026-07-10 23:05:19 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -363,7 +363,7 @@ applyHover(MinimizeBtn, Color3.fromRGB(180,180,180), Color3.fromRGB(255,150,30))
 applyHover(ClearSearchBtn, Color3.fromRGB(150,150,150), Color3.fromRGB(255,255,255))
 
 -- Логика Сворачивания / Разворачивания меню
-Library = {}
+local Library = {}
 Library.CurrentScale = 1.00
 local isMinimized = false
 
@@ -1011,7 +1011,7 @@ function Library:CreateToggle(parentPage, textKey, default, callback)
             tween(Indicator, {Position = UDim2.new(1, -16, 0.5, -7), BackgroundColor3 = activeIndicatorColor}, 0.2)
         else
             local bgL = (Library.CurrentThemeData.MainBg.R * 0.299 + Library.CurrentThemeData.MainBg.G * 0.587 + Library.CurrentThemeData.MainBg.B * 0.114)
-            local offColor = (bgL > 0.5) elder Color3.fromRGB(210, 210, 210) or Color3.fromRGB(40, 40, 40)
+            local offColor = (bgL > 0.5) and Color3.fromRGB(210, 210, 210) or Color3.fromRGB(40, 40, 40)
             tween(Checkbox, {BackgroundColor3 = offColor}, 0.2)
             tween(Indicator, {Position = UDim2.new(0, 2, 0.5, -7), BackgroundColor3 = Color3.fromRGB(255, 255, 255)}, 0.2)
         end
@@ -1327,7 +1327,7 @@ function Library:CreateSubTabs(parentPage, tabsList)
     return subPages
 end
 
-createPage = function(textKey, iconId, layoutOrder)
+local function createPage(textKey, iconId, layoutOrder)
     local initialText = Localization[Library.CurrentLanguage][textKey] or textKey
     local PageFrame = Instance.new("ScrollingFrame", PagesContainer)
     PageFrame.Size = UDim2.new(1, 0, 1, 0)
