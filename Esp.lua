c19efae4d538ffa18b45b3ba23119b4d6a1e6382
benzinmihail469-design <benzinmihail469@gmail.com>
2026-08-16 00:49:52 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,176 +1,296 @@
-local Decimals = 4
-local Clock = os.clock()
-local ValueText = "Value Is Now :"
+local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
+local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
+ 
+local Window = Library:CreateWindow{
+    Title = `Fluent {Library.Version}`,
+    SubTitle = "by Actual Master Oogway",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(830, 525),
+    Resize = true, -- Resize this ^ Size according to a 1920x1080 screen, good for mobile users but may look weird on some devices
+    MinSize = Vector2.new(470, 380),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.RightControl -- Used when theres no MinimizeKeybind
+}
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
-    cheatname = "Title Here", -- watermark text
-    gamename = "Title Here", -- watermark text
+-- Fluent Renewed provides ALL 1544 Lucide 0.469.0 https://lucide.dev/icons/ Icons and ALL 9072 Phosphor 2.1.0 https://phosphoricons.com/ Icons for the tabs, icons are optional
+local Tabs = {
+    Main = Window:CreateTab{
+        Title = "Main",
+        Icon = "phosphor-users-bold"
+    },
+    Settings = Window:CreateTab{
+        Title = "Settings",
+        Icon = "settings"
+    }
+}
+
+local Options = Library.Options
+
+Library:Notify{
+    Title = "Notification",
+    Content = "This is a notification",
+    SubContent = "SubContent", -- Optional
+    Duration = 5 -- Set to nil to make the notification not disappear
+}
+
+local Paragraph = Tabs.Main:CreateParagraph("Paragraph", {
+    Title = "Paragraph",
+    Content = "This is a paragraph.\nSecond line!"
 })
 
-library:init()
+print(Paragraph.Value)
 
-local Window1  = library.NewWindow({
-    title = "Title Here | Title Here", -- Mainwindow Text
-    size = UDim2.new(0, 510, 0.6, 6
-)})
+Paragraph:SetValue("This paragraph text is changed!")
 
-local Tab1 = Window1:AddTab("  Tab1  ")
-local SettingsTab = library:CreateSettingsTab(Window1)
+print(Paragraph.Value)
 
---Tab1:SetText("Text")
+Tabs.Main:CreateParagraph("Aligned Paragraph", {
+    Title = "Paragraph",
+    Content = "This is a paragraph with a center alignment!",
+    TitleAlignment = "Middle",
+    ContentAlignment = Enum.TextXAlignment.Center
+})
 
-local Section1 = Tab1:AddSection("Section 1", 1)
---Section1:SetText("Text")
-
-Section1:AddToggle({
-    text = "Toggle1",
-    state = false,
-    risky = true,
-    tooltip = "tooltip1",
-    flag = "Toggle_1",
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
+Tabs.Main:CreateButton{
+    Title = "Button",
+    Description = "Very important button",
+    Callback = function()
+        Window:Dialog{
+            Title = "Title",
+            Content = "This is a dialog",
+            Buttons = {
+                {
+                    Title = "Confirm",
+                    Callback = function()
+                        print("Confirmed the dialog.")
+                    end
+                },
+                {
+                    Title = "Cancel",
+                    Callback = function()
+                        print("Cancelled the dialog.")
+                    end
+                }
+            }
+        }
     end
-}):AddBind({
-    enabled = true,
-    text = "Keybind1",
-    tooltip = "tooltip1",
-    mode = "toggle",
-    bind = "None",
-    flag = "ToggleKey_1",
-    state = false,
-    nomouse = false,
-    risky = false,
-    noindicator = false,
-    callback = function(v)
-        print(ValueText, v)
+}
+
+local Toggle = Tabs.Main:CreateToggle("MyToggle", {Title = "Toggle", Default = false })
+
+Toggle:OnChanged(function()
+    print("Toggle changed:", Options.MyToggle.Value)
+end)
+
+Options.MyToggle:SetValue(false)
+
+local Slider = Tabs.Main:CreateSlider("Slider", {
+    Title = "Slider",
+    Description = "This is a slider",
+    Default = 2,
+    Min = 0,
+    Max = 5,
+    Rounding = 1,
+    Callback = function(Value)
+        print("Slider was changed:", Value)
+    end
+})
+
+Slider:OnChanged(function(Value)
+    print("Slider changed:", Value)
+end)
+
+Slider:SetValue(3)
+
+local Dropdown = Tabs.Main:CreateDropdown("Dropdown", {
+    Title = "Dropdown",
+    Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+    Multi = false,
+    Default = 1,
+})
+
+Dropdown:SetValue("four")
+
+Dropdown:OnChanged(function(Value)
+    print("Dropdown changed:", Value)
+end)
+
+local MultiDropdown = Tabs.Main:CreateDropdown("MultiDropdown", {
+    Title = "Dropdown",
+    Description = "You can select multiple values.",
+    Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+    Multi = true,
+    Default = {"seven", "twelve"},
+})
+
+MultiDropdown:SetValue{
+    three = true,
+    five = true,
+    seven = false
+}
+
+MultiDropdown:SetValues{"fifteen", "sixteen", "seventeen"}
+
+MultiDropdown:OnChanged(function(Value)
+    local Values = {}
+    for Value, State in next, Value do
+        Values[#Values + 1] = Value
+    end
+    print("Mutlidropdown changed:", table.concat(Values, ", "))
+end)
+
+local MultiInstanceDropdown = Tabs.Main:CreateDropdown("MultiInstanceDropdown", {
+    Title = "Instance Dropdown",
+    Description = "You can select multiple values and any instance or any other value!",
+    Values = {workspace, 5, Enum.JoinSource, Enum.MarketplaceBulkPurchasePromptStatus.Error},
+    Multi = true,
+    Default = {workspace},
+})
+
+MultiInstanceDropdown:OnChanged(function(Value)
+    local Values = {}
+    for Value, State in next, Value do
+        Values[#Values + 1] = typeof(Value)
+    end
+    print("Mutlidropdown with instance selection changed:", table.concat(Values, ", "))
+end)
+
+local Colorpicker = Tabs.Main:CreateColorpicker("Colorpicker", {
+    Title = "Colorpicker",
+    Default = Color3.fromRGB(96, 205, 255)
+})
+
+Colorpicker:OnChanged(function()
+    print("Colorpicker changed:", Colorpicker.Value)
+end)
+
+Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+
+local TColorpicker = Tabs.Main:CreateColorpicker("TransparencyColorpicker", {
+    Title = "Colorpicker",
+    Description = "but you can change the transparency.",
+    Transparency = 0,
+    Default = Color3.fromRGB(96, 205, 255)
+})
+
+TColorpicker:OnChanged(function()
+    print(
+        "TColorpicker changed:", TColorpicker.Value,
+        "Transparency:", TColorpicker.Transparency
+    )
+end)
+
+local Keybind = Tabs.Main:CreateKeybind("Keybind", {
+    Title = "KeyBind",
+    Mode = "Toggle", -- Always, Toggle, Hold
+    Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+    -- Occurs when the keybind is clicked, Value is `true`/`false`
+    Callback = function(Value)
+        print("Keybind clicked!", Value)
     end,
-    keycallback = function(v)
-        print(ValueText, v)
+
+    -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+    ChangedCallback = function(New)
+        print("Keybind changed!", New)
     end
 })
 
-Section1:AddBox({
-    enabled = true,
-    name = "TextBox1",
-    flag = "TextBox_1",
-    input = "PlaceHolder1",
-    focused = false,
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
+-- OnClick is only fired when you press the keybind and the mode is Toggle
+-- Otherwise, you will have to use Keybind:GetState()
+Keybind:OnClick(function()
+    print("Keybind clicked:", Keybind:GetState())
+end)
+
+Keybind:OnChanged(function()
+    print("Keybind changed:", Keybind.Value)
+end)
+
+task.spawn(function()
+    while true do
+        wait(1)
+
+        -- example for checking if a keybind is being pressed
+        local state = Keybind:GetState()
+        if state then
+            print("Keybind is being held down")
+        end
+
+        if Library.Unloaded then break end
+    end
+end)
+
+Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+
+local Input = Tabs.Main:CreateInput("Input", {
+    Title = "Input",
+    Default = "Default",
+    Placeholder = "Placeholder",
+    Numeric = false, -- Only allows numbers
+    Finished = false, -- Only calls callback when you press enter
+    Callback = function(Value)
+        print("Input changed:", Value)
     end
 })
 
---[[Section1:AddToggle({
-    text = "Toggle1",
-    flag = "Toggle_1",
-    callback = function(v)
-        print(ValueText, v)
+Input:OnChanged(function()
+    print("Input updated:", Input.Value)
+end)
+
+Tabs.Main:CreateButton{
+    Title = "Really Really big Dropdown",
+    Description = "",
+    Callback = function()
+        local Values = {}
+
+        for i = 1, 1000 do
+            Values[i] = i
+        end
+
+        Tabs.Main:AddDropdown("BIGDropdown", {
+            Title = "Big Dropdown",
+            Values = Values,
+            Multi = false,
+            Default = 1,
+        })
     end
-}):AddColor({
-    text = "Color1",
-    color = Color3.fromRGB(255, 255, 255),
-    flag = "Color_1",
-    callback = function(v)
-        print(ValueText, v)
-    end
-})--]]
+}
 
---[[Section1:AddBind({
-    enabled = true,
-    text = "Keybind1",
-    tooltip = "tooltip1",
-    mode = "toggle",
-    bind = "None",
-    flag = "ToggleKey_1",
-    state = false,
-    nomouse = false,
-    risky = false,
-    noindicator = false,
-    callback = function(v)
-        print(ValueText, v)
-    end,
-    keycallback = function(v)
-        print(ValueText, v)
-    end
-})--]]
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
 
-Section1:AddSeparator({
-    enabled = true,
-    text = "Separator1"
-})
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Library)
+InterfaceManager:SetLibrary(Library)
 
-Section1:AddButton({
-    enabled = true,
-    text = "Button1",
-    tooltip = "tooltip1",
-    confirm = true,
-    risky = false,
-    callback = function()
-        print("Pressed!")
-    end
-})
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
 
--- Button1:SetText("Text")
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes{}
 
-Section1:AddSlider({
-    enabled = true,
-    text = "Slider1",
-    tooltip = "tooltip1",
-    flag = "Slider_1",
-    suffix = "",
-    dragging = true,
-    focused = false,
-    min = 0,
-    max = 100,
-    increment = 0.1,
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
-})
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
 
---Slider_1:SetValue(100)
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
 
-Section1:AddList({
-    enabled = true,
-    text = "Selection", 
-    tooltip = "tooltip1",
-    selected = "",
-    multi = false,
-    open = false,
-    max = 4,
-    values = {"1", "2", "3"},
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
-})
 
-Section1:AddColor({
-    enabled = true,
-    text = "Color1",
-    tooltip = "tooltip1",
-    color = Color3.fromRGB(255, 255, 255),
-    flag = "Color_1",
-    trans = 0,
-    open = false,
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
-})
+Window:SelectTab(1)
 
-local Time = (string.format("%."..tostring(Decimals).."f", os.clock() - Clock))
-library:SendNotification(("Loaded In "..tostring(Time)), 6)
+Library:Notify{
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
+}
 
---[[
-    --Window1:SetOpen(false)
-    makefolder("Title Here")
-    library:SetTheme(Default)
-    library:GetConfig(Default)
-    library:LoadConfig(Default)
-    library:SaveConfig(Default)
-]]
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
