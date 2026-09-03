607707a7d66fe1e96ccd1a9472af7715566b0f23
benzinmihail469-design <benzinmihail469@gmail.com>
2026-09-03 22:20:33 +0300

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -308,7 +308,7 @@ local Library = {
 
 function Library:CreateWindow(data)
     data = data or {}
-    local logoId = data.Logo or "91905309683896"
+    local logoId = data.Logo or "134603867443812" -- Обновленная иконка
 
     local Window = {
         LeftTabs = nil,
@@ -2851,7 +2851,7 @@ end
 -- =======================================================
 
 local MainWindow = Library:CreateWindow({
-    Logo = "91905309683896" -- Обновленная иконка
+    Logo = "134603867443812" -- Обновленная иконка
 })
 
 -- 1. Вкладка Combat
